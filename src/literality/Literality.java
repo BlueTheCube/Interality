66abc94a499633c5c7b3d88819bb6a9d77860fc9
Cubical box <67639725+Calreiboz@users.noreply.github.com>
2022-12-15 16:38:10 +0800

    fard

---
+++ src/literality/Literality.java
@@ -0,0 +1,22 @@
+package literality;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+
+public class ExampleJavaMod extends Mod{
+
+    public ExampleJavaMod(){
+    
+    }
+
+    @Override
+    public void loadContent(){
+        Log.info("Loading literal content.");
+    }
+
+}
